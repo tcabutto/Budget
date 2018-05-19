121eccc078176dab26d5900123da7385c6b6bf68
Tyler Cabutto <tacabutto@valdosta.edu>
2018-05-19 17:31:57 -0400

    Finished account class.

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,76 @@
+import java.text.DecimalFormat;
 
-public class Account {
+public class Account 
+{
 	private String name;
+	private double balance;
+	private boolean negative;
+	private DecimalFormat df = new DecimalFormat("#.00"); 
+	
+	public Account()
+	{
+		setNegative(false);
+	}
+	
+	
+	public String getName() 
+	{
+		return name;
+	}
+
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+
+	public double getBalance() 
+	{
+		return balance;
+	}
+
+
+	public void setBalance(double balance) 
+	{
+		this.balance = balance;
+	}
+	
+	public void isNegative() 
+	{
+		if(negative)
+		{
+			System.out.println("Your account is in the negative!\nBalance: $" + df.format(balance));
+		}
+		else
+		{
+			System.out.println("You're still in the positive!\nBalance: $" + df.format(balance));
+		}
+	}
+
+
+	public void setNegative(boolean val)
+	{
+		this.negative = val;
+	}
+	
+	
+	public void add(double cash)
+	{
+		balance += cash;
+	}
+	
+	public void subtract(double cash)
+	{
+		balance -= cash;
+	}
+	
+	public String toString()
+	{
+
+		String retval = "Account name: " + name + "\n" + "Account balance: $" + df.format(balance);
+
+		return retval;
+	}
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,50 @@
+import java.util.ArrayList;
 
-public class Person {
+public class Person 
+{
+	private String fname, lname;
+	private ArrayList<Account> accounts;
+	
+	public Person()
+	{
+		accounts = new ArrayList<Account>();
+	}
+	
+	public void addAccount(Account a)
+	{
+		accounts.add(a);
+	}
+	
+	public void getAccount(String name)
+	{
+		for(int i=0; i<accounts.size(); i++)
+		{
+			Account find = accounts.get(i);
+			if(find.getName().equals(name))
+			{	//Will print information for account rather than passing data back to main.
+				System.out.println(find.toString());
+				break;
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
 
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
 }
